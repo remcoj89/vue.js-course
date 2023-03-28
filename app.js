5866684b972daf69d5bab95081c684aa633f6414
remco jonk <remcojonk@hotmail.com>
2023-03-28 20:55:10 +0200

    two way data binding and binding attributes

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const vm = Vue.createApp({
   data() {
     return {
       firstName: 'Remco',
-      lastName: 'Jonk'
+      lastName: 'Jonk',
+      url: 'https://www.remcojonk.me',
+      raw_url: `<a href="https://google.com" target="_blank">Raw HTML Link</a>`
     }
   },
   methods: {
     fullName() {
-      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+      return `${this.firstName} ${this.lastName}`;
     }
   }
 }).mount('#app')
