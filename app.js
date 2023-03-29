36aa698459010b6fa63c1fffc1df4d465732732a
remco jonk <remcojonk@hotmail.com>
2023-03-29 15:20:11 +0200

    finished Vue fundementals

--- app.js
+++ app.js
@@ -2,15 +2,45 @@ const vm = Vue.createApp({
   data() {
     return {
       firstName: 'Remco',
+      middleName: '',
       lastName: 'Jonk',
       url: 'https://www.remcojonk.me',
-      raw_url: `<a href="https://google.com" target="_blank">Raw HTML Link</a>`
+      age: 20,
+      isPurple: false,
+      isBlue: false,
+      selectedColor: '',
+      circle: "circle",
+      size: 150,
+      mode: 1,
+      items: ["pizza", "lasgna", "Spagethi"],
+      itemsObjects: [{restaurant: "La Baracca"}, {restaurant: "La Fabrica"}]
     }
   },
   methods: {
-    fullName() {
-      return `${this.firstName} ${this.lastName}`;
+    increment() {
+      this.age++
+    },
+    updateMiddleName(event) {
+      this.middleName = event.target.value
     }
+  },
+  computed: {
+    fullName() {
+      console.log("updated fullName")
+      return `${this.firstName} ${this.middleName} ${this.lastName}`;
+    },
+    circle_classes() {
+      return { blue: this.isBlue, purple: this.isPurple }
+    },
+    circleSize() {
+      return  {width: this.size + 'px', height: this.size + `px`, lineHeight: this.size + 'px'}
+    },
+    transform() {
+     return { transform: 'rotate(30deg)' }
+    }
+  },
+  watch: {
+
   }
 }).mount('#app')
 
